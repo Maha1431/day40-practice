495b51a4c6d26aca78d4ff4ccb58d1462fbedc9a
maha <mmahaece07@gmail.com>
2021-12-18 13:21:13 +0530

    Map files

---
+++ Map/dictionary.js
@@ -0,0 +1,10 @@
+const products = [
+    { name: 'Laptop', price: 32000, brand: 'Lenovo', color: 'Silver' },
+    { name: 'Phone', price: 700, brand: 'Iphone', color: 'Golden' },
+    { name: 'Watch', price: 3000, brand: 'Casio', color: 'Yellow' },
+    { name: 'Aunglass', price: 300, brand: 'Ribon', color: 'Blue' },
+    { name: 'Camera', price: 9000, brand: 'Lenovo', color: 'Gray' },
+];
+
+const productName = products.map(product => product.name);
+console.log(productName);
